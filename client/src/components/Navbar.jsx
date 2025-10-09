b63d4474514420873fc07d7279dd57a93f2e41f1
ibnu-3 <ibnuturab3@gmail.com>
2025-10-09 08:34:25 +0000

    navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import useAuth from '../context/useAuth'
 
 const Navbar = () => {
+  const {user}=useAuth()
   return (
-    <div>Navbar</div>
+    <div className='fixed w-full flex items-center justify-between bg-slate-200 border-b shadow-md text-slate-500 p-4'>
+      <h1>E-commerce</h1>
+      <div>
+        {!user && <div>
+          <button className='bg-blue-500'>Logout</button></div>}
+      </div>
+    </div>
   )
 }
 
