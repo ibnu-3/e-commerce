b02ebdb8db68d8f63829e0a9720e4491b7b13d74
ibnu-3 <ibnuturab3@gmail.com>
2025-10-19 08:36:26 +0000

    product page updated

--- client/src/pages/Admin/Products.jsx
+++ client/src/pages/Admin/Products.jsx
@@ -8,24 +8,24 @@ const Products = () => {
   return (
     <DashboardLayout activeMenu={'Products'}>
     <div className=''>
-       <table className='overflow-x-auto'>
+       <table className='overflow-x-auto w-full'>
         <thead>
-            <tr className='flex items-center justify-between'>
-                <th className='px-4 py-2 border-b '>Name</th>
-                <th className='px-4 py-2 border-b '>Category</th>
-                <th className='px-4 py-2 border-b '>Price</th>
-                <th className='px-4 py-2 border-b '>In Stock</th>
-                <th className='px-4 py-2 border-b '>Actions</th>
+            <tr className='flex items-center justify-between border'>
+                <th className='px-4 py-2 border-l '>Name</th>
+                <th className='px-4 py-2 border-l '>Category</th>
+                <th className='px-4 py-2 border-l '>Price</th>
+                <th className='px-4 py-2 border-l '>In Stock</th>
+                <th className='px-4 py-2 border-l '>Actions</th>
             </tr>
         </thead>
         <tbody>
             {products.map((item)=>(
-                <tr className='flex items-center justify-between capitalize' key={item._id}>
-                <td className='px-4 py-2 border-b '>{item.name}</td>
-                <td className='px-4 py-2 border-b '>{item.category}</td>
-                <td className='px-4 py-2 border-b '>{item.newPrice.toFixed(2)}</td>
-                <td className='px-4 py-2 border-b '>{item.inStock} </td>
-                <td className='px-4 py-2 border-b flex items-center gap-3'><MdEdit className='text-green-500'/> <MdDelete className='text-red-500' /></td>
+                <tr className='flex items-center justify-between capitalize border-b odd:bg-slate-100' key={item._id}>
+                <td className='px-4 py-2 '>{item.name}</td>
+                <td className='px-4 py-2 '>{item.category}</td>
+                <td className='px-4 py-2 '>{item.newPrice.toFixed(2)}</td>
+                <td className='px-4 py-2 '>{item.countInStock} </td>
+                <td className='px-4 py-2 flex items-center gap-3'><MdEdit className='text-green-500'/> <MdDelete className='text-red-500' /></td>
             </tr>
             ))}
         </tbody>
